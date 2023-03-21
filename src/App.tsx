5f0a54d918cce41f53635dbb56245764b7560bc8
Jiali Zhang <zhangjiali86@gmail.com>
2023-03-21 14:02:49 +0100

    feat: container

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { Select } from "./Select"
+
+const options=[
+  {label:"First", value: "1"},
+  {label:"Second", value: "2"},
+  {label:"Third", value: "3"},
+  {label:"Fourth", value: "4"},
+  {label:"Fifth", value: "5"},
+  {label:"Sixth", value: "6"},
+]
+
 function App() {
 
   return (
     <div className="App">
-
+      <Select options={options}/>
     </div>
   )
 }
--- src/select.tsx
+++ src/select.tsx
@@ -11,4 +11,20 @@ type SelectProps = {
 
 export function Select({ value, onChange, options }: SelectProps) {
 
+    return(
+        <div tabIndex={0} 
+        className="relative w-80 min-h-6 border-2 border-solid  border-yellow-500 flex items-center gap-2 p-2 rounded focus:border-yellow-200">
+            <span className="value">value</span>
+            <button className="clear-btn">&times;</button>
+            <div className="divider"></div>
+            <div className="caret"></div>
+            <ul className="options">
+                {options.map(option => (
+                    <li key={option.label} className="option">
+                        {option.label}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
