9a962ecce393a4bb61d7e802677d880c6c96766e
Jiali Zhang <zhangjiali86@gmail.com>
2023-03-29 22:00:11 +0200

    import daisyUI

--- src/select.tsx
+++ src/select.tsx
@@ -14,8 +14,8 @@ export function Select({ value, onChange, options }: SelectProps) {
     return(
         <div tabIndex={0} 
         className="relative w-80 min-h-6 border-2 border-solid  border-yellow-500 flex items-center gap-2 p-2 rounded focus:border-yellow-200">
-            <span className="value">value</span>
-            <button className="clear-btn">&times;</button>
+            <span className="grow">value</span>
+            <button className="btn btn-outline btn-warning btn-xs">&times;</button>
             <div className="divider"></div>
             <div className="caret"></div>
             <ul className="options">
