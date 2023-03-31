6baab463b2889e91018cbfa596ed876f01af7c66
Jiali Zhang <zhangjiali86@gmail.com>
2023-03-31 10:30:17 +0200

    add options css

--- src/select.tsx
+++ src/select.tsx
@@ -1,3 +1,5 @@
+import './custom.css'
+
 type SelectOption = {
     label: string,
     value: string,
@@ -12,7 +14,7 @@ type SelectProps = {
 export function Select({ value, onChange, options }: SelectProps) {
 
     return (
-        <div>
+        <div className="flex justify-center items-center min-h-screen">
             <div tabIndex={0}
                 className="relative w-80 min-h-6 border-2 border-solid  border-yellow-400 flex items-center gap-2 p-2 rounded group focus:border-yellow-200">
                 <span className="grow">value</span>
@@ -20,8 +22,8 @@ export function Select({ value, onChange, options }: SelectProps) {
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                 </button>
                 <div className="bg-yellow-400 group-focus:bg-yellow-200 self-stretch w-0.5"></div>
-                <div className="translate-x-0 translate-y-1/4 border-8 border-solid border-transparent border-t-yellow-400"></div>
-                <ul className="m-0 p-0 max-h-60">
+                <div className="translate-x-0 translate-y-1/4 border-8 border-solid border-transparent border-t-yellow-400 group-focus:border-t-yellow-200"></div>
+                <ul className="absolute-position m-0 p-0 max-h-60 overflow-y-auto border-2 border-solid border-yellow-400 rounded w-full">
                     {options.map(option => (
                         <li key={option.label} className="option">
                             {option.label}
