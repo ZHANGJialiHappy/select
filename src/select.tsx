7492e1657be6e0dbe5b0ab0d5ea93f5458427768
Jiali Zhang <zhangjiali86@gmail.com>
2023-04-01 22:01:57 +0200

    select option

--- src/select.tsx
+++ src/select.tsx
@@ -19,6 +19,10 @@ export function Select({ value, onChange, options }: SelectProps) {
         onChange(undefined)
     }
 
+    const selectOption = (option:SelectOption) => {
+        onChange(option)
+    }
+
     return (
         <div className="flex justify-center items-center min-h-screen">
             <div tabIndex={0}
@@ -28,9 +32,9 @@ export function Select({ value, onChange, options }: SelectProps) {
             >
                 <span className="grow">{value?.label}</span>
                 <button className="btn btn-circle btn-outline btn-warning btn-xs"
-                    onClick= {event=>{
-                        event.stopPropagation()
-                        clearOptions()
+                    onClick = {event=>{
+                        event.stopPropagation();
+                        clearOptions();
                     }}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                 </button>
@@ -38,7 +42,12 @@ export function Select({ value, onChange, options }: SelectProps) {
                 <div className="translate-x-0 translate-y-1/4 border-8 border-solid border-transparent border-t-yellow-400 group-focus:border-t-yellow-200"></div>
                 <ul className={`absolute-position m-0 p-0 max-h-60 overflow-y-auto border-2 border-solid border-yellow-200 rounded w-full bg-white z-50 ${isOpen? "" : "hidden"}`}>
                     {options.map(option => (
-                        <li key={option.label} className="px-2 py-1 cursor-pointer">
+                        <li onClick = {event=>{
+                            event.stopPropagation();
+                            selectOption(option);
+                            setIsOpen(false);
+                        }}
+                        key={option.label} className="px-2 py-1 cursor-pointer">
                             {option.label}
                         </li>
                     ))}
