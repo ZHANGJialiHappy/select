8759d46b30d3b8cc982da47b8698eab6534ebbd9
Jiali Zhang <zhangjiali86@gmail.com>
2023-04-03 16:06:22 +0200

    feat: multiple selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,55 @@
 import { useState } from "react"
-import { Select } from "./Select"
+import { Select, SelectOption } from "./Select"
 
-const options=[
-  {label:"First", value: "1"},
-  {label:"Second", value: "2"},
-  {label:"Third", value: "3"},
-  {label:"Fourth", value: "4"},
-  {label:"Fifth", value: "5"},
-  {label:"Sixth", value: "6"},
-  {label:"Seventh", value: "7"},
-  {label:"Eighth", value: "8"},
-  {label:"First", value: "9"},
-  {label:"Second", value: "10"},
-  {label:"Third", value: "11"},
-  {label:"Fourth", value: "12"},
-  {label:"Fifth", value: "13"},
-  {label:"Sixth", value: "14"},
-  {label:"Seventh", value: "15"},
-  {label:"Eighth", value: "16"},
-  {label:"First", value: "17"},
-  {label:"Second", value: "18"},
-  {label:"Third", value: "19"},
-  {label:"Fourth", value: "20"},
-  {label:"Fifth", value: "21"},
-  {label:"Sixth", value: "22"},
-  {label:"Seventh", value: "23"},
-  {label:"Eighth", value: "24"},
+const options = [
+  { label: "First", value: "1" },
+  { label: "Second", value: "2" },
+  { label: "Third", value: "3" },
+  { label: "Fourth", value: "4" },
+  { label: "Fifth", value: "5" },
+  { label: "Sixth", value: "6" },
+  { label: "Seventh", value: "7" },
+  { label: "Eighth", value: "8" },
+  { label: "First", value: "9" },
+  { label: "Second", value: "10" },
+  { label: "Third", value: "11" },
+  { label: "Fourth", value: "12" },
+  { label: "Fifth", value: "13" },
+  { label: "Sixth", value: "14" },
+  { label: "Seventh", value: "15" },
+  { label: "Eighth", value: "16" },
+  { label: "First", value: "17" },
+  { label: "Second", value: "18" },
+  { label: "Third", value: "19" },
+  { label: "Fourth", value: "20" },
+  { label: "Fifth", value: "21" },
+  { label: "Sixth", value: "22" },
+  { label: "Seventh", value: "23" },
+  { label: "Eighth", value: "24" },
 
 ]
 
 function App() {
-  const [value, setValue] = useState<typeof options[0] | undefined>(options[0])
+  const [value1, setValue1] = useState<SelectOption[]>([options[0]])
+  const [value2, setValue2] = useState<SelectOption | undefined>(options[0])
+
 
   return (
-    <div className="App">
-      <Select options={options} value={value} onChange={option => setValue(option)}/>
+    <div className="flex justify-center items-center min-h-screen">
+      <div>
+        <h1 className="text-2xl mb-3">MULTIPLE SELECTOR</h1>
+        <Select
+          multiple
+          options={options}
+          value={value1}
+          onChange={option => setValue1(option)} />
+
+        <div className="text-2xl mb-3 mt-5">SINGLE SELECTOR</div>
+        <Select
+          options={options}
+          value={value2}
+          onChange={option => setValue2(option)} />
+      </div>
     </div>
   )
 }
--- src/select.tsx
+++ src/select.tsx
@@ -1,64 +1,96 @@
 import { useState } from 'react'
 import './custom.css'
 
-type SelectOption = {
+export type SelectOption = {
     label: string,
     value: string,
 }
 
-type SelectProps = {
-    options: SelectOption[],
+type MultipleSelectProps = {
+    multiple: true,
+    value: SelectOption[],
+    onChange: (value: SelectOption[]) => void
+}
+
+type SingleSelectProps = {
+    multiple?: false,
     value?: SelectOption,
     onChange: (value: SelectOption | undefined) => void
 }
 
-export function Select({ value, onChange, options }: SelectProps) {
+type SelectProps = {
+    options: SelectOption[],
+} & (SingleSelectProps | MultipleSelectProps)
+
+export function Select({ multiple, value, onChange, options }: SelectProps) {
     const [isOpen, setIsOpen] = useState<boolean>(false)
 
     const clearOptions = () => {
-        onChange(undefined)
+        multiple ? onChange([]) : onChange(undefined)
     }
 
     const selectOption = (option: SelectOption) => {
-        onChange(option)
+        if (multiple) {
+            if (value?.includes(option)) {
+                onChange(value.filter(option => option !== option))
+            } else {
+                onChange([...value, option])
+            }
+        } else {
+            if (option !== value) onChange(option)
+        }
     }
 
     const isOptionSelected = (option: SelectOption) => {
-        return option === value;
+        return multiple ? value.includes(option) : option === value;
     }
 
     return (
-        <div className="flex justify-center items-center min-h-screen">
             <div tabIndex={0}
                 className="relative w-80 min-h-6 border-2 border-solid border-yellow-400 flex items-center gap-2 p-2 rounded group focus:border-yellow-200"
                 onClick={() => setIsOpen(true)}
                 onBlur={() => setIsOpen(false)}
             >
-                <span className="grow">{value?.label}</span>
+                <span className="grow">{
+                    multiple ?
+                        value.map(v => (
+                        <button 
+                            key={v.value}
+                            onClick={e => {
+                                e.stopPropagation();
+                                selectOption(v);
+                            }}
+                            className="btn btn-warning btn-sm"
+                            >{v.label}
+                            <span className="text-yellow-200">&times;</span>
+                        </button>))
+                        : 
+                        value?.label}
+                </span>
                 <button className="btn btn-circle btn-outline btn-warning btn-xs"
-                    onClick = {event=>{
-                        event.stopPropagation();
+                    onClick={e => {
+                        e.stopPropagation();
                         clearOptions();
                     }}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                 </button>
                 <div className="bg-yellow-400 group-focus:bg-yellow-200 self-stretch w-0.5"></div>
                 <div className="translate-x-0 translate-y-1/4 border-8 border-solid border-transparent border-t-yellow-400 group-focus:border-t-yellow-200"></div>
-                <ul className={`absolute-position m-0 p-0 max-h-60 overflow-y-auto border-2 border-solid border-yellow-200 rounded w-full bg-white z-50 ${isOpen? "" : "hidden"}`}>
+                <ul className={`absolute-position m-0 p-0 max-h-60 overflow-y-auto border-2 border-solid border-yellow-200 rounded w-full bg-white z-50 ${isOpen ? "" : "hidden"}`}>
                     {options.map((option) => (
-                        <li onClick = {event=>{
+                        <li onClick={event => {
                             event.stopPropagation();
                             selectOption(option);
                             setIsOpen(false);
                         }}
-                        key={option.value} 
-                        className={`${isOptionSelected(option)? "p-2 cursor-pointer bg-yellow-400" : "p-2 cursor-pointer hover:bg-yellow-200"}`}
+                            key={option.value}
+                            className={`${isOptionSelected(option) ? "p-2 cursor-pointer bg-yellow-400" : "p-2 cursor-pointer hover:bg-yellow-200"}`}
                         >
                             {option.label}
                         </li>
                     ))}
                 </ul>
             </div>
-        </div>
+
     )
 }
